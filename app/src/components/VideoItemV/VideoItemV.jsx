e38138cba6f7a78c65491d3279c8183b55e7e3f8
Brandon Benoit <brandon@digitalkatana.dev>
2026-07-18 10:24:37 -0700

    updated list item action buttons

--- app/src/components/VideoItemV/VideoItemV.jsx
+++ app/src/components/VideoItemV/VideoItemV.jsx
@@ -3,10 +3,18 @@ import { Link } from 'react-router-dom';
 import Paper from '../Paper';
 import './itemV.scss';
 
-const VideoItemV = ({ link, onClick, elevation, image, alt, caption }) => {
+const VideoItemV = ({
+	link,
+	onClick,
+	itmClass,
+	elevation,
+	image,
+	alt,
+	caption,
+}) => {
 	return (
 		<Link to={link} className='v-link' onClick={onClick}>
-			<Paper className='v-item' elevation={elevation}>
+			<Paper className={`v-item ${itmClass}`} elevation={elevation}>
 				<img src={image} alt={alt} />
 			</Paper>
 			<h6 className='responsive-h6 caption'>{caption}</h6>
