2f57e7434953e329cce40b30f5c64de6657a7c0d
Brandon <brandon@digitalkatana.dev>
2026-08-22 07:09:50 -0700

    working on favorites

--- app/src/redux/slices/videoSlice.js
+++ app/src/redux/slices/videoSlice.js
@@ -1,7 +1,7 @@
 import {
-	createAsyncThunk,
-	createEntityAdapter,
-	createSlice,
+  createAsyncThunk,
+  createEntityAdapter,
+  createSlice,
 } from '@reduxjs/toolkit';
 import { PURGE } from 'redux-persist';
 import { logout } from './userSlice';
@@ -9,136 +9,141 @@ import { shuffleArray } from '../../util/helpers';
 import nutzflixApi from '../../api/nutflixApi';
 
 export const getVideos = createAsyncThunk(
-	'video/get_videos',
-	async (_, { rejectWithValue }) => {
-		try {
-			const res = await nutzflixApi.get('/api/videos');
-			return res.data;
-		} catch (err) {
-			return rejectWithValue(err.response.data);
-		}
-	},
+  'video/get_videos',
+  async (_, { rejectWithValue }) => {
+    try {
+      const res = await nutzflixApi.get('/api/videos');
+      return res.data;
+    } catch (err) {
+      return rejectWithValue(err.response.data);
+    }
+  },
 );
 
 export const videoAdapter = createEntityAdapter();
 const initialState = videoAdapter.getInitialState({
-	loading: false,
-	allVideos: [],
-	movies: [],
-	series: [],
-	lastFetched: null,
-	featured: null,
-	selectedVideo: null,
-	selectedSeries: null,
-	selectedSeason: null,
-	searchTerm: '',
-	searchResults: [],
-	videoSuccess: null,
-	videoErrors: null,
+  loading: false,
+  allVideos: [],
+  movies: [],
+  series: [],
+  lastFetched: null,
+  featured: null,
+  favorites: [],
+  selectedVideo: null,
+  selectedSeries: null,
+  selectedSeason: null,
+  searchTerm: '',
+  searchResults: [],
+  videoSuccess: null,
+  videoErrors: null,
 });
 
 export const videoSlice = createSlice({
-	name: 'video',
-	initialState,
-	reducers: {
-		setVideos: (state, action) => {
-			state.allVideos = action.payload.allVideos;
-			state.featured = shuffleArray(action.payload.movies)[0];
-			state.movies = action.payload.movies;
-			state.series = action.payload.series;
-			state.lastFetched = Date.now();
-		},
-		setFeatured: (state, action) => {
-			state.featured = action.payload;
-		},
-		setSelectedVideo: (state, action) => {
-			state.selectedVideo = action.payload;
-		},
-		setSelectedSeries: (state, action) => {
-			state.selectedSeries = action.payload;
-		},
-		setSelectedSeason: (state, action) => {
-			state.selectedSeason = action.payload;
-		},
-		setSearchTerm: (state, action) => {
-			state.searchTerm = action.payload;
-		},
-		videoSearch: (state, action) => {
-			const queryWords = action.payload
-				.toLowerCase()
-				.split(' ')
-				.filter(Boolean);
-			state.searchResults = state.allVideos.filter((video) => {
-				const title = video.title.toLowerCase() ?? '';
-				return queryWords.every((word) => title.includes(word));
-			});
-		},
-		movieSearch: (state, action) => {
-			const queryWords = action.payload
-				.toLowerCase()
-				.split(' ')
-				.filter(Boolean);
-			state.searchResults = state.movies.filter((movie) => {
-				const title = movie.title.toLowerCase() ?? '';
-				return queryWords.every((word) => title.includes(word));
-			});
-		},
-		seriesSearch: (state, action) => {
-			const queryWords = action.payload
-				.toLowerCase()
-				.split(' ')
-				.filter(Boolean);
-			state.searchResults = state.series.filter((series) => {
-				const title = series.title.toLowerCase() ?? '';
-				return queryWords.every((word) => title.includes(word));
-			});
-		},
-		clearSearchResults: (state) => {
-			state.searchResults = [];
-		},
-		clearAllSelected: (state) => {
-			state.selectedVideo = null;
-			state.selectedSeries = null;
-			state.selectedSeason = null;
-			state.searchTerm = '';
-			state.searchResults = [];
-		},
-	},
-	extraReducers: (builder) => {
-		builder
-			.addCase(getVideos.pending, (state) => {
-				state.loading = true;
-				state.videoErrors = null;
-			})
-			.addCase(getVideos.fulfilled, (state, action) => {
-				state.loading = false;
-				state.allVideos = action.payload.allVideos ?? [];
-				state.movies = action.payload.movies ?? [];
-				state.series = action.payload.series ?? [];
-				state.lastFetched = Date.now();
-			})
-			.addCase(getVideos.rejected, (state, action) => {
-				state.loading = false;
-				state.videoErrors = action.payload;
-			})
-			.addCase(logout, () => {
-				return initialState;
-			});
-	},
+  name: 'video',
+  initialState,
+  reducers: {
+    setVideos: (state, action) => {
+      state.allVideos = action.payload.allVideos;
+      state.featured = shuffleArray(action.payload.movies)[0];
+      state.movies = action.payload.movies;
+      state.series = action.payload.series;
+      state.lastFetched = Date.now();
+    },
+    setFeatured: (state, action) => {
+      state.featured = action.payload;
+    },
+    setFavorites: (state, action) => {
+      state.favorites = action.payload ?? [];
+    },
+    setSelectedVideo: (state, action) => {
+      state.selectedVideo = action.payload;
+    },
+    setSelectedSeries: (state, action) => {
+      state.selectedSeries = action.payload;
+    },
+    setSelectedSeason: (state, action) => {
+      state.selectedSeason = action.payload;
+    },
+    setSearchTerm: (state, action) => {
+      state.searchTerm = action.payload;
+    },
+    videoSearch: (state, action) => {
+      const queryWords = action.payload
+        .toLowerCase()
+        .split(' ')
+        .filter(Boolean);
+      state.searchResults = state.allVideos.filter((video) => {
+        const title = video.title.toLowerCase() ?? '';
+        return queryWords.every((word) => title.includes(word));
+      });
+    },
+    movieSearch: (state, action) => {
+      const queryWords = action.payload
+        .toLowerCase()
+        .split(' ')
+        .filter(Boolean);
+      state.searchResults = state.movies.filter((movie) => {
+        const title = movie.title.toLowerCase() ?? '';
+        return queryWords.every((word) => title.includes(word));
+      });
+    },
+    seriesSearch: (state, action) => {
+      const queryWords = action.payload
+        .toLowerCase()
+        .split(' ')
+        .filter(Boolean);
+      state.searchResults = state.series.filter((series) => {
+        const title = series.title.toLowerCase() ?? '';
+        return queryWords.every((word) => title.includes(word));
+      });
+    },
+    clearSearchResults: (state) => {
+      state.searchResults = [];
+    },
+    clearAllSelected: (state) => {
+      state.selectedVideo = null;
+      state.selectedSeries = null;
+      state.selectedSeason = null;
+      state.searchTerm = '';
+      state.searchResults = [];
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getVideos.pending, (state) => {
+        state.loading = true;
+        state.videoErrors = null;
+      })
+      .addCase(getVideos.fulfilled, (state, action) => {
+        state.loading = false;
+        state.allVideos = action.payload.allVideos ?? [];
+        state.movies = action.payload.movies ?? [];
+        state.series = action.payload.series ?? [];
+        state.lastFetched = Date.now();
+      })
+      .addCase(getVideos.rejected, (state, action) => {
+        state.loading = false;
+        state.videoErrors = action.payload;
+      })
+      .addCase(logout, () => {
+        return initialState;
+      });
+  },
 });
 
 export const {
-	setVideos,
-	setSearchTerm,
-	videoSearch,
-	movieSearch,
-	seriesSearch,
-	setFeatured,
-	setSelectedVideo,
-	setSelectedSeries,
-	setSelectedSeason,
-	clearSearchResults,
-	clearAllSelected,
+  setVideos,
+  setSearchTerm,
+  videoSearch,
+  movieSearch,
+  seriesSearch,
+  setFeatured,
+  setFavorites,
+  setSelectedVideo,
+  setSelectedSeries,
+  setSelectedSeason,
+  clearSearchResults,
+  clearAllSelected,
 } = videoSlice.actions;
 
 export default videoSlice.reducer;
--- app/src/views/User/UserHome/UserHome.jsx
+++ app/src/views/User/UserHome/UserHome.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import {
-	setSelectedSeries,
-	setSelectedVideo,
-	setSearchTerm,
-	clearSearchResults,
+  setSelectedSeries,
+  setSelectedVideo,
+  setSearchTerm,
+  clearSearchResults,
 } from '../../../redux/slices/videoSlice';
 import { shuffleArray, buildGenreLists } from '../../../util/helpers';
 import Paper from '../../../components/Paper';
@@ -14,70 +14,75 @@ import Carousel from '../../../components/Carousel';
 import './userhome.scss';
 
 const UserHome = () => {
-	const { drawerOpen } = useSelector((state) => state.app);
-	const {featured, movies, series, searchResults } = useSelector((state) => state.video);
-	const dispatch = useDispatch();
-	const lists = buildGenreLists(movies);
+  const { drawerOpen } = useSelector((state) => state.app);
+  const { featured, movies, series, searchResults, favorites } = useSelector(
+    (state) => state.video,
+  );
+  const dispatch = useDispatch();
+  const lists = buildGenreLists(movies);
 
-	const handleSelectedVideo = (video) => {
-		if (video.videoType.toLowerCase() === 'series') {
-			dispatch(setSelectedSeries(video));
-		} else if (video.videoType.toLowerCase() === 'movie') {
-			dispatch(setSelectedVideo(video));
-		}
-		dispatch(setSearchTerm(''));
-		dispatch(clearSearchResults());
-	};
+  const handleSelectedVideo = (video) => {
+    if (video.videoType.toLowerCase() === 'series') {
+      dispatch(setSelectedSeries(video));
+    } else if (video.videoType.toLowerCase() === 'movie') {
+      dispatch(setSelectedVideo(video));
+    }
+    dispatch(setSearchTerm(''));
+    dispatch(clearSearchResults());
+  };
 
-	const handleFeaturedClick = ()=> {
-		dispatch(setSelectedVideo(featured));
-	}
+  const handleFeaturedClick = () => {
+    dispatch(setSelectedVideo(featured));
+  };
 
-	return (
-		<div className='home'>
-			{!drawerOpen && searchResults.length > 0 ? (
-				<div className='search-wrapper'>
-					{searchResults.map((r) => (
-						<Link
-							to={
-								r.videoType.toLowerCase() === 'series'
-									? '/series-details'
-									: r.videoType.toLowerCase() === 'movie' && '/video-details'
-							}
-							key={r._id}
-							onClick={() => handleSelectedVideo(r)}
-						>
-							<div className='result-item-wrapper'>
-								<Paper className='poster-wrapper' elevation={5}>
-									<img
-										src={
-											r.videoType.toLowerCase() === 'series'
-												? r.folder
-												: r.poster
-										}
-										alt={r.title}
-									/>
-								</Paper>
-								<h6>{r.title}</h6>
-							</div>
-						</Link>
-					))}
-				</div>
-			) : (
-				<>
-					<Trailer featured video={featured} onClick={handleFeaturedClick}/>
-					<div className='carousel-wrapper'>
-						<Carousel series list={shuffleArray(series)} arrows count={20} />
-						{lists
-							.filter((list) => list.movies.length > 0)
-							.map((list) => (
-								<Carousel key={list.name} list={list} arrows />
-							))}
-					</div>
-				</>
-			)}
-		</div>
-	);
+  return (
+    <div className='home'>
+      {!drawerOpen && searchResults.length > 0 ? (
+        <div className='search-wrapper'>
+          {searchResults.map((r) => (
+            <Link
+              to={
+                r.videoType.toLowerCase() === 'series'
+                  ? '/series-details'
+                  : r.videoType.toLowerCase() === 'movie' && '/video-details'
+              }
+              key={r._id}
+              onClick={() => handleSelectedVideo(r)}
+            >
+              <div className='result-item-wrapper'>
+                <Paper className='poster-wrapper' elevation={5}>
+                  <img
+                    src={
+                      r.videoType.toLowerCase() === 'series'
+                        ? r.folder
+                        : r.poster
+                    }
+                    alt={r.title}
+                  />
+                </Paper>
+                <h6>{r.title}</h6>
+              </div>
+            </Link>
+          ))}
+        </div>
+      ) : (
+        <>
+          <Trailer featured video={featured} onClick={handleFeaturedClick} />
+          <div className='carousel-wrapper'>
+            {favorites.length && (
+              <Carousel favs list={shuffleArray(favorites)} arrows />
+            )}
+            <Carousel series list={shuffleArray(series)} arrows count={20} />
+            {lists
+              .filter((list) => list.movies.length > 0)
+              .map((list) => (
+                <Carousel key={list.name} list={list} arrows />
+              ))}
+          </div>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default UserHome;
